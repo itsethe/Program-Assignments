ba23da14f48560e1c6e6b563fb6d6b7637928f1a
Ethan So <EtheStarGaming@gmail.com>
2024-06-24 20:11:03 -0700

    finished lab 2

--- OneDrive/Desktop/Program Assignments/fundamentals_practice.py
+++ OneDrive/Desktop/Program Assignments/fundamentals_practice.py
@@ -1,2 +1,27 @@
 print("Hello World")
 #
+
+name = "LeBron"
+#String var
+age = 35
+#int var
+salary = 37.44
+#float var
+plays_basketball = True
+#boolean var
+jersey_number = "23"
+#String var
+
+car_name = "Mclaren"
+x = 50
+
+my_first_name = "John"
+
+print(name)
+print(age)
+print(salary)
+print(plays_basketball)
+print(jersey_number)
+
+print(car_name)
+print(my_first_name)
